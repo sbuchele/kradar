4c1f61bd431f078cabdcab89f91beb75aa48c3d5
Torcas <torcasbmb@gmail.com>
2013-11-10 01:53:48 -0600

    wee

--- Kradar/src/com/example/kradar/MainActivity.java
+++ Kradar/src/com/example/kradar/MainActivity.java
@@ -34,41 +34,41 @@ public class MainActivity extends FragmentActivity
 
 	Controller c;
 
-//	// Global constants
-//	/*
-//	 * Define a request code to send to Google Play services
-//	 * This code is returned in Activity.onActivityResult
-//	 */
-//	private final static int
-//	CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-//
-//	// Define a DialogFragment that displays the error dialog
-//	public static class ErrorDialogFragment extends DialogFragment {
-//		// Global field to contain the error dialog
-//		private Dialog mDialog;
-//		// Default constructor. Sets the dialog field to null
-//		public ErrorDialogFragment() {
-//			super();
-//			mDialog = null;
-//		}
-//		// Set the dialog to display
-//		public void setDialog(Dialog dialog) {
-//			mDialog = dialog;
-//		}
-//		// Return a Dialog to the DialogFragment.
-//		@Override
-//		public Dialog onCreateDialog(Bundle savedInstanceState) {
-//			return mDialog;
-//		}
-//	}
+	//	// Global constants
+	//	/*
+	//	 * Define a request code to send to Google Play services
+	//	 * This code is returned in Activity.onActivityResult
+	//	 */
+	//	private final static int
+	//	CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	//
+	//	// Define a DialogFragment that displays the error dialog
+	//	public static class ErrorDialogFragment extends DialogFragment {
+	//		// Global field to contain the error dialog
+	//		private Dialog mDialog;
+	//		// Default constructor. Sets the dialog field to null
+	//		public ErrorDialogFragment() {
+	//			super();
+	//			mDialog = null;
+	//		}
+	//		// Set the dialog to display
+	//		public void setDialog(Dialog dialog) {
+	//			mDialog = dialog;
+	//		}
+	//		// Return a Dialog to the DialogFragment.
+	//		@Override
+	//		public Dialog onCreateDialog(Bundle savedInstanceState) {
+	//			return mDialog;
+	//		}
+	//	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		c=new Controller();
-		
-		
+
+
 		//button creation and implementation
 		Button b1 = (Button) findViewById(R.id.attack);
 		Button b2 = (Button) findViewById(R.id.Friends);
@@ -76,7 +76,7 @@ public class MainActivity extends FragmentActivity
 		b1.setOnClickListener(Click1());
 		b2.setOnClickListener(Click2());
 		b3.setOnClickListener(Click3());
-		
+
 		startService(new Intent(this, KradarLocService.class));
 	}
 
@@ -92,140 +92,141 @@ public class MainActivity extends FragmentActivity
 	public void onStart()
 	{
 		super.onStart();
-		
-	
+
+
+		KradarLocService.fluffy.poke();
 
 		//if(Fluffles.Messenger. == false)
 		//{
-			
+
 		//}
 	}
 
 
-//	/*
-//	 * Handle results returned to the FragmentActivity
-//	 * by Google Play services
-//	 */
-//	@Override
-//	protected void onActivityResult(
-//			int requestCode, int resultCode, Intent data) {
-//		// Decide what to do based on the original request code
-//		switch (requestCode) {
-//		case CONNECTION_FAILURE_RESOLUTION_REQUEST :
-//			/*
-//			 * If the result code is Activity.RESULT_OK, try
-//			 * to connect again
-//			 */
-//			switch (resultCode) {
-//			case Activity.RESULT_OK :
-//				/*
-//				 * Try the request again
-//				 */
-//
-//				break;
-//			}
-//
-//		}
-//	}
+	//	/*
+	//	 * Handle results returned to the FragmentActivity
+	//	 * by Google Play services
+	//	 */
+	//	@Override
+	//	protected void onActivityResult(
+	//			int requestCode, int resultCode, Intent data) {
+	//		// Decide what to do based on the original request code
+	//		switch (requestCode) {
+	//		case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+	//			/*
+	//			 * If the result code is Activity.RESULT_OK, try
+	//			 * to connect again
+	//			 */
+	//			switch (resultCode) {
+	//			case Activity.RESULT_OK :
+	//				/*
+	//				 * Try the request again
+	//				 */
+	//
+	//				break;
+	//			}
+	//
+	//		}
+	//	}
 
-//	private boolean servicesConnected() {
-//		// Check that Google Play services is available
-//		int resultCode =
-//				GooglePlayServicesUtil.
-//				isGooglePlayServicesAvailable(this);
-//		// If Google Play services is available
-//		if (ConnectionResult.SUCCESS == resultCode) {
-//			// In debug mode, log the status
-//			Log.d("Location Updates",
-//					"Google Play services is available.");
-//			// Continue
-//			return true;
-//			// Google Play services was not available for some reason
-//		} else {
-//			// Get the error code
-//			int errorCode = connectionResult.getErrorCode();
-//			// Get the error dialog from Google Play services
-//			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
-//					errorCode,
-//					this,
-//					CONNECTION_FAILURE_RESOLUTION_REQUEST);
-//
-//			// If Google Play services can provide an error dialog
-//			if (errorDialog != null) {
-//				// Create a new DialogFragment for the error dialog
-//				ErrorDialogFragment errorFragment =
-//						new ErrorDialogFragment();
-//				// Set the dialog in the DialogFragment
-//				errorFragment.setDialog(errorDialog);
-//				// Show the error dialog in the DialogFragment
-//				errorFragment.show(getSupportFragmentManager(),
-//						"Location Updates");
-//			}
-//		}
-//	}
-//
-//	/*
-//	 * Called by Location Services when the request to connect the
-//	 * client finishes successfully. At this point, you can
-//	 * request the current location or start periodic updates
-//	 */
-//	@Override
-//	public void onConnected(Bundle dataBundle) {
-//		// Display the connection status
-//		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-//
-//	}
-//
-//	/*
-//	 * Called by Location Services if the connection to the
-//	 * location client drops because of an error.
-//	 */
-//	@Override
-//	public void onDisconnected() {
-//		// Display the connection status
-//		Toast.makeText(this, "Disconnected. Please re-connect.",
-//				Toast.LENGTH_SHORT).show();
-//	}
-//
-//	@Override
-//	public void onLocationChanged(Location location) {
-//		// Report to the UI that the location was updated
-//        String msg = "Updated Location: " +
-//                Double.toString(location.getLatitude()) + "," +
-//                Double.toString(location.getLongitude());
-//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-//
-//
-//	}
-//
-//
-//	@Override
-//	public void onProviderDisabled(String provider) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//
-//	@Override
-//	public void onProviderEnabled(String provider) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//
-//	@Override
-//	public void onStatusChanged(String provider, int status, Bundle extras) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//
-//	@Override
-//	public void onConnectionFailed(ConnectionResult arg0) {
-//		// TODO Auto-generated method stub
-//
-//	}
-	
+	//	private boolean servicesConnected() {
+	//		// Check that Google Play services is available
+	//		int resultCode =
+	//				GooglePlayServicesUtil.
+	//				isGooglePlayServicesAvailable(this);
+	//		// If Google Play services is available
+	//		if (ConnectionResult.SUCCESS == resultCode) {
+	//			// In debug mode, log the status
+	//			Log.d("Location Updates",
+	//					"Google Play services is available.");
+	//			// Continue
+	//			return true;
+	//			// Google Play services was not available for some reason
+	//		} else {
+	//			// Get the error code
+	//			int errorCode = connectionResult.getErrorCode();
+	//			// Get the error dialog from Google Play services
+	//			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+	//					errorCode,
+	//					this,
+	//					CONNECTION_FAILURE_RESOLUTION_REQUEST);
+	//
+	//			// If Google Play services can provide an error dialog
+	//			if (errorDialog != null) {
+	//				// Create a new DialogFragment for the error dialog
+	//				ErrorDialogFragment errorFragment =
+	//						new ErrorDialogFragment();
+	//				// Set the dialog in the DialogFragment
+	//				errorFragment.setDialog(errorDialog);
+	//				// Show the error dialog in the DialogFragment
+	//				errorFragment.show(getSupportFragmentManager(),
+	//						"Location Updates");
+	//			}
+	//		}
+	//	}
+	//
+	//	/*
+	//	 * Called by Location Services when the request to connect the
+	//	 * client finishes successfully. At this point, you can
+	//	 * request the current location or start periodic updates
+	//	 */
+	//	@Override
+	//	public void onConnected(Bundle dataBundle) {
+	//		// Display the connection status
+	//		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+	//
+	//	}
+	//
+	//	/*
+	//	 * Called by Location Services if the connection to the
+	//	 * location client drops because of an error.
+	//	 */
+	//	@Override
+	//	public void onDisconnected() {
+	//		// Display the connection status
+	//		Toast.makeText(this, "Disconnected. Please re-connect.",
+	//				Toast.LENGTH_SHORT).show();
+	//	}
+	//
+	//	@Override
+	//	public void onLocationChanged(Location location) {
+	//		// Report to the UI that the location was updated
+	//        String msg = "Updated Location: " +
+	//                Double.toString(location.getLatitude()) + "," +
+	//                Double.toString(location.getLongitude());
+	//        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	//
+	//
+	//	}
+	//
+	//
+	//	@Override
+	//	public void onProviderDisabled(String provider) {
+	//		// TODO Auto-generated method stub
+	//
+	//	}
+	//
+	//
+	//	@Override
+	//	public void onProviderEnabled(String provider) {
+	//		// TODO Auto-generated method stub
+	//
+	//	}
+	//
+	//
+	//	@Override
+	//	public void onStatusChanged(String provider, int status, Bundle extras) {
+	//		// TODO Auto-generated method stub
+	//
+	//	}
+	//
+	//
+	//	@Override
+	//	public void onConnectionFailed(ConnectionResult arg0) {
+	//		// TODO Auto-generated method stub
+	//
+	//	}
+
 	public OnClickListener Click1()
 	{
 		OnClickListener click = new OnClickListener()
@@ -233,13 +234,13 @@ public class MainActivity extends FragmentActivity
 			@Override
 			public void onClick(View V)
 			{
-				
+
 			}
-			
+
 		};
 		return click;
 	}
-		
+
 	public OnClickListener Click2()
 	{
 		OnClickListener click = new OnClickListener()
@@ -250,11 +251,11 @@ public class MainActivity extends FragmentActivity
 				Intent intent = new Intent(MainActivity.this, F_list.class);
 				MainActivity.this.startActivity(intent);
 			}
-			
+
 		};
 		return click;
 	}
-	
+
 	public OnClickListener Click3()
 	{
 		OnClickListener click = new OnClickListener()
@@ -266,14 +267,14 @@ public class MainActivity extends FragmentActivity
 				MainActivity.this.startActivity(intent);
 			}			
 		};
-		
+
 		return click;
 	}
 
 	public String getMyPhoneNumber()
 	{
-	    return ((TelephonyManager) getSystemService(TELEPHONY_SERVICE))
-	            .getLine1Number();
+		return ((TelephonyManager) getSystemService(TELEPHONY_SERVICE))
+				.getLine1Number();
 	}
-	
+
 }
