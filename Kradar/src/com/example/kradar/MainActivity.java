29b81a4d4ea052eb97b0bd05286d45f95d32c3ff
sbuchele <ksbuchele@gmail.com>
2013-11-10 13:31:51 -0600

    Location Services

--- Kradar/src/com/example/kradar/MainActivity.java
+++ Kradar/src/com/example/kradar/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.kradar;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.DialogFragment;
@@ -11,17 +19,22 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends FragmentActivity 
-//implements
-//GooglePlayServicesClient.ConnectionCallbacks,
-//GooglePlayServicesClient.OnConnectionFailedListener,
-//LocationListener  
+implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener,
+LocationListener  
 {
 
 	final public static String PHONE_NUMBER = "com.example.kradar.PhoneNumber" ;
 	static boolean first = false;
+	
+	LocationClient mLocationClient;
+	
+	boolean inProgress = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +62,7 @@ public class MainActivity extends FragmentActivity
 		b2.setOnClickListener(Click2());
 		b3.setOnClickListener(Click3());
 
+		mLocationClient = new LocationClient(this, this, this);
 
 	}
 
@@ -60,32 +74,47 @@ public class MainActivity extends FragmentActivity
 		return true;
 	}
 
-	@Override
-	public void onStart()
-	{
-		super.onStart();
-<<<<<<< HEAD
-=======
-		
->>>>>>> cd69676b7b95026cde168f8ddb59ff387d2cd1b0
-//		KradarLocService.fluffy.tophat.setPhone(Controller.pref.getString(PHONE_NUMBER, "3"));
-//		KradarLocService.fluffy.poke();
+	
+	Location mLocation;
+	Context here = this;
 
-	}
 	public OnClickListener Click1()
 	{
-		SharedPreferences Power= Controller.pref;
+		
 		OnClickListener click = new OnClickListener()
 		{
 			@Override
 			public void onClick(View V)
 			{
+				if(servicesConnected()){
+				mLocation = mLocationClient.getLastLocation();
 				
+		        Toast.makeText(here, mLocation.getLatitude() + "  " + mLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+		        
+		        KradarLocService.fluffy.tophat.setLat(mLocation.getLatitude());
+		        KradarLocService.fluffy.poke();
+		        
+		       	KradarLocService.fluffy.tophat.setLon(mLocation.getLongitude());
+		        KradarLocService.fluffy.poke();
+			}
 			}
 
 		};
 		return click;
 	}
+	
+private boolean servicesConnected() {
+    	
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 	public OnClickListener Click2()
 	{
@@ -115,5 +144,42 @@ public class MainActivity extends FragmentActivity
 		};
 
 		return click;
+	}
+
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		String msg = "Updated Location: " +
+                Double.toString(arg0.getLatitude()) + "," +
+                Double.toString(arg0.getLongitude());
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        
+        KradarLocService.fluffy.tophat.setLat(arg0.getLatitude());
+        KradarLocService.fluffy.poke();
+        
+       	KradarLocService.fluffy.tophat.setLon(arg0.getLongitude());
+        KradarLocService.fluffy.poke();
+		
+	}
+
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		inProgress = false;
+	}
+
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		mLocation = mLocationClient.getLastLocation();
+	}
+
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
 	}
 }
