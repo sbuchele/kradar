90a10ee8b247cbb1b833eeed9531b013e0468b57
Zac <flichin@hotmail.com>
2013-11-09 18:14:09 -0600

    FLUFFY FLUFFLES

--- Kradar/src/com/example/kradar/Fluffles.java
+++ Kradar/src/com/example/kradar/Fluffles.java
@@ -1,5 +1,46 @@
 package com.example.kradar;
 
-public class Fluffles {
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class Fluffles extends Activity {//Server/Client code
+
+	public class Messenger implements Runnable{
+
+	@Override
+	 public void run() {
+        boolean connected;
+		try {
+            InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
+            Log.d("ClientActivity", "C: Connecting...");
+            Socket socket = new Socket(serverAddr, ServerActivity.SERVERPORT);
+            connected = true;
+            while (connected) {
+                try {
+                    Log.d("ClientActivity", "C: Sending command.");
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                                .getOutputStream())), true);
+                        // where you issue the commands
+                        out.println("Hey Server!");
+                        Log.d("ClientActivity", "C: Sent.");
+                } catch (Exception e) {
+                    Log.e("ClientActivity", "S: Error", e);
+                }
+            }
+            socket.close();
+            Log.d("ClientActivity", "C: Closed.");
+        } catch (Exception e) {
+            Log.e("ClientActivity", "C: Error", e);
+            connected = false;
+        }
+    }
+
+	}
 }
