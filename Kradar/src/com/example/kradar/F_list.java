eb4d785b943976821d77c96da73f2647f79ac00e
Torcas <torcasbmb@gmail.com>
2013-11-10 00:57:48 -0600

    update

--- Kradar/src/com/example/kradar/F_list.java
+++ Kradar/src/com/example/kradar/F_list.java
@@ -1,5 +1,13 @@
 package com.example.kradar;
 
-public class F_list {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+public class F_list extends FragmentActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list);
+	
+	
+	}
 }
