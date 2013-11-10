42a6f62654b943d116cddab8b03a9ac1a312c679
SpaceTW <spacetw@att.net>
2013-11-10 09:28:10 -0600

    Minor list enhancements

--- Kradar/src/com/example/kradar/F_list.java
+++ Kradar/src/com/example/kradar/F_list.java
@@ -1,17 +1,33 @@
 package com.example.kradar;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.ListView;
 
 public class F_list extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list);
 		
+		final ListView listview = (ListView) findViewById(R.id.friendList);
+		String magicList=null;//TODO
+		Scanner read=new Scanner(magicList);
+		final ArrayList<String> list = new ArrayList<String>();
 		
-		//Scanner read=new Scanner(null);
+		while(read.hasNext()){
+			list.add(read.next());
+		}
+	    
+	    String[] values = new String[list.size()];
+	    for (int i=0; i < list.size(); i++) {
+	      values[i]=list.get(i);
+	    }
+		
+		
+		
 		
 	
 	}
