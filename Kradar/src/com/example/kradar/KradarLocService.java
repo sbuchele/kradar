c5147d26c1586ab283fb0aef5e7594b1536fe392
SpaceTW <spacetw@att.net>
2013-11-09 22:12:39 -0600

    Location services hopefully works

--- Kradar/src/com/example/kradar/KradarLocService.java
+++ Kradar/src/com/example/kradar/KradarLocService.java
@@ -1,33 +1,148 @@
 package com.example.kradar;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 
-public class KradarLocService extends IntentService{
+public class KradarLocService extends Service implements 
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener,
+	LocationListener{
     
 	public final String LOCATION_INTENT="com.example.kradar.Return Loc";
 	public final String LOCATION_EXTRA="com.example.kradar.location";
 	private SharedPreferences prefs;
+    private LocationClient mLocationClient;
+    private LocationRequest mLocationRequest;
+    // Flag that indicates if a request is underway.
+    private boolean mInProgress;
+    private Boolean servicesAvailable = false;
 	
-	public KradarLocService(String name) {
-		super(name);
+	public KradarLocService() {
+		super();
 		Context ctx = getApplicationContext();
 		prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
 	}
+    
+    @Override
+	public void onCreate() {
+        super.onCreate();
+        
+ 
+        mInProgress = false;
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use moderately high accuracy
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        // Set the update interval to 5 min
+        mLocationRequest.setInterval(1000*60*5);
+        // Set the fastest update interval to 2.5 min
+        mLocationRequest.setFastestInterval(1000*30*5);
+        
+        servicesAvailable = servicesConnected();
+        
+        /*
+         * Create a new location client, using the enclosing class to
+         * handle callbacks.
+         */
+        mLocationClient = new LocationClient(this, this, this);
+    }
+	
+    private boolean servicesConnected() {
+    	
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    @Override
+    public int onStartCommand (Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        
+        if(!servicesAvailable || mLocationClient.isConnected() || mInProgress)
+        	return START_STICKY;
+        
+        if(mLocationClient == null) 
+            mLocationClient = new LocationClient(this, this, this);
+        if(!mLocationClient.isConnected() || !mLocationClient.isConnecting() && !mInProgress)
+        {
+        	mInProgress = true;
+        	mLocationClient.connect();
+        }
+        
+        return START_STICKY;
+    }
+	
+	public class LocalBinder extends Binder {
+    	public KradarLocService getServerInstance() {
+    		return KradarLocService.this;
+    	}
+    }
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return new LocalBinder();
+	}
 
 	@Override
-    protected void onHandleIntent(Intent workIntent) {
-        // Gets data from the incoming Intent
-        String dataString = workIntent.getDataString();
-        
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
-        // Do work here, based on the contents of dataString
-        
-        
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		mInProgress = false;
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		 // Request location updates using static settings
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+	}
+
+	@Override
+	public void onDisconnected() {
+		// Turn off the request flag
+        mInProgress = false;
+        // Destroy the current location client
+        mLocationClient = null;
+	}
+	
+    @Override
+    public void onDestroy(){
+        // Turn off the request flag
+        mInProgress = false;
+        if(servicesAvailable && mLocationClient != null) {
+	        mLocationClient.removeLocationUpdates(this);
+	        // Destroy the current location client
+	        mLocationClient = null;
+        }
+        super.onDestroy();  
     }
+	
+	
 	
 }
