0966fefcb8d04f211f1d111a782dd8a147ec631c
Zac <flichin@hotmail.com>
2013-11-10 05:37:42 -0600

    gweasfgawes

--- Kradar/src/com/example/kradar/Fluffles.java
+++ Kradar/src/com/example/kradar/Fluffles.java
@@ -43,13 +43,18 @@ public class Fluffles implements Runnable {//Server/Client code
                     .getOutputStream())), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println("Entering Fluffles Loop");
+            out.println("testestestest");
             while (connected) {
+            	out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
+                        .getOutputStream())), true);
+            	in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             	try {
             		dialogue = null;
-					if (in.ready()) {
+					/*if (in.ready()) {
 						dialogue = in.readLine();
+						in.
 						System.out.println("Had dialouge!");
-					}
+					}*/
 				} catch (Exception e) {
 					Log.e("ClientActivity", "S: Error", e);
 				}
