b761b9e55d657a857a4037956e0a61a82b5359b3
SpaceTW <spacetw@att.net>
2013-11-10 00:12:49 -0600

    Minor changes

--- Kradar/src/com/example/kradar/Throw.java
+++ Kradar/src/com/example/kradar/Throw.java
@@ -2,15 +2,23 @@ package com.example.kradar;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.EditText;
 
 public class Throw extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.adder);
+		EditText entry=(EditText) findViewById(R.id.phone_number);
+		String phoneNum=entry.getText().toString();
 
-		
 		Hats tricorn = new Hats();
+		
 		
 
 	}
+	
+	
+	
+	
+	 
 }
