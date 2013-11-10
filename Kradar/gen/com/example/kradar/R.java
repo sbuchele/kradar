9c3578720a5b43195f2a951ff143ce60116475a3
Torcas <torcasbmb@gmail.com>
2013-11-09 18:54:28 -0600

    buttons

--- Kradar/gen/com/example/kradar/R.java
+++ Kradar/gen/com/example/kradar/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int Friends=0x7f080004;
+        public static final int action_settings=0x7f080007;
+        public static final int add=0x7f080006;
+        public static final int attack=0x7f080005;
+        public static final int charge=0x7f080002;
+        public static final int chargebar=0x7f080003;
         public static final int textView1=0x7f080001;
         public static final int textView2=0x7f080000;
     }
@@ -36,11 +41,11 @@ public final class R {
     }
     public static final class string {
         public static final int Attack=0x7f050003;
+        public static final int Charge=0x7f050006;
         public static final int Friends=0x7f050004;
         public static final int Updates=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int charge=0x7f050006;
         public static final int introScreen=0x7f050002;
     }
     public static final class style {
