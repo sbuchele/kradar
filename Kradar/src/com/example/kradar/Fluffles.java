74b85b0994bfa31885cee35e452ddb6737af90b6
Zac <flichin@hotmail.com>
2013-11-10 07:50:23 -0600

    Tiny change

--- Kradar/src/com/example/kradar/Fluffles.java
+++ Kradar/src/com/example/kradar/Fluffles.java
@@ -43,7 +43,7 @@ public class Fluffles implements Runnable {//Server/Client code
                     .getOutputStream())), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println("Entering Fluffles Loop");
-            out.println("testestestest");
+            //out.println("testestestest");
             while (connected) {
             	out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket
                         .getOutputStream())), true);
